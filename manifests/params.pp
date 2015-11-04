98dc0fc0a82a3bed555b9c9bc3600bf303241ba7
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2015-11-04 15:44:31 +0100

    Fixed typo which prevented correct execution on CentOS

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,12 +22,15 @@ class impala::params {
     'RedHat': {
       $packages = {
         common => [ 'impala' ],
-        catalogd => 'impala-catalog',
+        catalog => 'impala-catalog',
         server => 'impala-server',
         statestore => 'impala-state-store',
         frontend => 'impala-shell',
       }
       $daemons = {
+        catalog => 'impala-catalog',
+        statestore => 'impala-state-store',
+        server => 'impala-server',
       }
     }
     default: {
